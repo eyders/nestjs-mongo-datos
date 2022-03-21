1716b93cdf35ba0991e45e92aaa7627bafbd3448
eyders <eyders@gmail.com>
2022-03-21 11:07:07 -0500

    Configuracion y conexion a la DB

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,13 @@ export default registerAs('config', () => {
       port: process.env.DATABASE_PORT,
     },
     apiKey: process.env.API_KEY,
+    mongo: {
+      dbName: process.env.MONGO_DB,
+      user: process.env.MONGO_INITDB_ROOT_USERNAME,
+      password: process.env.MONGO_INITDB_ROOT_PASSWORD,
+      port: parseInt(process.env.MONGO_PORT, 10),
+      host: process.env.MONGO_HOST,
+      connection: process.env.MONGO_CONNECTION,
+    },
   };
 });
